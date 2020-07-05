e2cbcd94338d4fbd76fa360196dc92084548e429
williangbezerra <williangomesbezerra@gmail.com>
2020-07-05 19:58:50 -0300

    ContaList view design

--- src/gui/MainViewController.java
+++ src/gui/MainViewController.java
@@ -31,7 +31,7 @@ public class MainViewController implements Initializable {
 
 	@FXML
 	public void onMenuItemPlanodeContasAction() {
-		System.out.println("onMenuItemPlanodeContasAction");
+		loadView("/gui/ContaList.fxml");
 	}
 
 	@FXML
